572549ab9125e63cc4553756e34c56bf0303ba60
Vinuthna-G-1091 <cs1231091@cse.iitd.ac.in>
2025-02-28 14:46:55 +0530

    fixed

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
+
 #include<stdio.h>
+// using namespace std;
+
 int main(int*argc,int *argv){
     if(*argc!=2){
         printf("%d",*argc);
     }else{
         printf("%d %d",argv[0],argv[1]);
     }
+    return 0;
 }
